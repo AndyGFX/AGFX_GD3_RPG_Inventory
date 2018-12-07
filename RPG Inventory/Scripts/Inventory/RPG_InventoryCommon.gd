9c577e0242f69dfe174c9f8ff68d4e292767cc48
AndyGFX <o.kollar@gmail.com>
2018-12-07 07:18:23 +0100

    Cleanup code

--- RPG Inventory/Scripts/Inventory/RPG_InventoryCommon.gd
+++ RPG Inventory/Scripts/Inventory/RPG_InventoryCommon.gd
@@ -312,3 +312,47 @@ func LoadSlots():
 	pass
 
 
+# -------------------------------------------------------------------------------
+# PLAYER: OnInput event
+# -------------------------------------------------------------------------------
+
+func OnInput(event):
+	
+	
+	
+	if RPG_InventoryCommon.holdingItem != null && RPG_InventoryCommon.holdingItem.picked:		
+		RPG_InventoryCommon.SetHoldingItemPosition(event.position)
+		pass
+
+
+
+# -------------------------------------------------------------------------------
+# PLAYER: OnGuiInput event
+# -------------------------------------------------------------------------------
+
+func OnGuiInput(event):
+	
+	# LEFT MOUSE BUTTON PRESSED
+	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT and event.pressed and RPG_InventoryCommon.disableInventory==false:
+		# check item under mouse cursor
+		RPG_InventoryCommon.CheckItemUnderMouse()
+		
+		# if item is picked ...
+		if RPG_InventoryCommon.holdingItem != null:
+			# put on full slot => SWAP items
+			if RPG_InventoryCommon.clickedSlot.item != null: RPG_InventoryCommon.SwapItemsInSlot()
+			# put on empty slot => INSERT item
+			else: RPG_InventoryCommon.PutHoldingItemToSlot()
+		# if item isn't picked => pickup new one
+		elif RPG_InventoryCommon.clickedSlot.item != null: RPG_InventoryCommon.PickItemFromSlot()
+		pass
+	
+	# RIGHT MOUSE BUTTON PRESSED
+	if event is InputEventMouseButton and event.button_index == BUTTON_RIGHT and event.pressed and RPG_InventoryCommon.holdingItem == null:
+		
+		RPG_InventoryCommon.CheckItemUnderMouse()
+		# show button panel only for when mouse is over inventory slot with item
+		if RPG_InventoryCommon.clickedSlot != null and RPG_InventoryCommon.clickedSlot.slotType == "InventorySlot":
+			RPG_InventoryCommon.ShowCommands()
+		
+		pass
